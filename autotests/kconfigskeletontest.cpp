67a97c6da4f6bf394c5f2637b9a9cf3f5ab45ae7
Felix Resch <resch.felix@gmail.com>
2019-11-19 13:31:45 +0100

    WIP: added headers to cmake targets

--- autotests/kconfigskeletontest.cpp
+++ autotests/kconfigskeletontest.cpp
@@ -23,6 +23,12 @@
 #include <QFont>
 #include <QtTestGui>
 
+#ifdef false
+#ifdef FEAT_ELEKTRA
+#include <kdb.hpp>
+#endif
+#endif
+
 QTEST_MAIN(KConfigSkeletonTest)
 
 #define DEFAULT_SETTING1 false
@@ -65,6 +71,19 @@ void KConfigSkeletonTest::cleanup()
 {
     delete s;
     //TODO delete keys from Elektra DB
+#ifdef false
+#ifdef FEAT_ELEKTRA
+    KDB kdb;
+
+    KeySet keySet;
+
+    kdb.get(keySet, "user/sw/org/kde/kconfigskeletontestrc/#5/current/");
+
+    keySet.cut(Key("user/sw/org/kde/kconfigskeletontestrc/#5/current/", KEY_END));
+
+    kdb.set(keySet, "user/sw/org/kde/kconfigskeletontestrc/#5/current/");
+#endif
+#endif
 }
 
 void KConfigSkeletonTest::testSimple()
